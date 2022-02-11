457be68bd32bbe688d30827185d89d9c428b01dc
liyanhrxy <sweepmonkli@gmail.com>
2022-02-11 14:56:39 +0800

    fix: ble about code adjustment

--- packages/kit/src/index.tsx
+++ packages/kit/src/index.tsx
@@ -5,3 +5,4 @@ export { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 export type { PayloadAction } from '@reduxjs/toolkit';
 export { default as Provider } from './provider';
 export { default as useNavigation } from './hooks/useNavigation';
+export { default as bleUtils } from './utils/ble/utils';
